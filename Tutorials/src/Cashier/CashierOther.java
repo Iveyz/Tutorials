64f55b0e80a3cbec8cd3d9f4b108785802ec114f
Iveyz <iveyz80@gmail.com>
2013-05-23 18:02:22 -0400

    startedhw
    Began the HW

---
+++ Tutorials/src/Cashier/CashierOther.java
@@ -0,0 +1,23 @@
+package Cashier;
+
+public class CashierOther {
+
+            
+        String name;
+        int age;
+        int yearsWorked;
+        
+        
+        System.out.println("Our new cashier is " + name + "who is " + age + "and has worked for " + yearsWorked + "years.");
+    }
+
+
+
+//public CashierOther(String newName, int newAge, int newYearsWorked){
+//        
+//        name = newName;
+//        age = newAge;
+//        yearsWorked = newYearsWorked;
+//        System.out.println("Our new cashier is " + name + " that is " + newAge + " with " + newYearsWorked);
+    
+    
